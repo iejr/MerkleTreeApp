4312fbc0c7a2859673aa0c2183454860f2356c2d
iejr <15151370+iejr@users.noreply.github.com>
2025-02-26 00:25:24 +0900

    [MerkleTreeApi] Add User ID Query Api
    - But doesn't work due to stateless nature of the controller, will
      checkout other ways

--- MerkleTreeApi/Controllers/UserBalanceController.cs
+++ MerkleTreeApi/Controllers/UserBalanceController.cs
@@ -8,6 +8,10 @@ public class CreateRequest {
     public string? branch_tag { get; set; }
 }
 
+public class GetBalanceRequest {
+    public int user_id { get; set; }
+}
+
 public class User {
     public int id { get; set;}
     public int balance { get; set; }
@@ -24,18 +28,34 @@ public class UserBalanceController : ControllerBase
     private readonly ILogger<UserBalanceController> _logger;
 
     private  MerkleTree.MerkleTree _merkle_tree;
+    private Dictionary<int, User> _user_records;
 
     public UserBalanceController(ILogger<UserBalanceController> logger)
     {
         _logger = logger;
         _merkle_tree = new MerkleTree.MerkleTree();
+        _user_records = new Dictionary<int, User>();
     }
 
     [HttpPost("create")]
-    // public ActionResult<string> Create() {
     public IActionResult Create([FromBody] CreateRequest request) {
+        this._user_records = request.user_data.ToDictionary(e => e.id, e => e);
+        // Console.WriteLine("Show user id {0}", request.user_data[0].id);
+
         string[] payloads = request.user_data.ConvertAll(e => e.Serialization()).ToArray(); 
         string result = _merkle_tree.Build(payloads, request.leaf_tag!, request.branch_tag!);
+        return Ok(new { Status = "Ok", Details = result });;
+    }
+
+    [HttpPost("getbalance")]
+    public IActionResult GetBalance([FromBody] GetBalanceRequest request) {
+        // Console.WriteLine("Show user id {0}", request.user_id);
+        if (!this._user_records.ContainsKey(request.user_id)) {
+            return NotFound(new { Message = "User Id not found" });
+        }
+
+        string query = this._user_records[request.user_id].Serialization();
+        var result = _merkle_tree.Proof(query);
         return Ok(new { Status = "Ok", Details = result });;
     }
 }
