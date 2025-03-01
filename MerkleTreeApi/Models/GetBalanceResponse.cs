11d157a6b47e8bcc8ff44aed5caaa4d6513f277c
iejr <15151370+iejr@users.noreply.github.com>
2025-03-01 20:56:26 +0900

    [CodeStyle] Add comments in code

--- MerkleTreeApi/Models/GetBalanceResponse.cs
+++ MerkleTreeApi/Models/GetBalanceResponse.cs
@@ -1,6 +1,10 @@
 namespace MerkleTreeApi.Models;
 
 public class GetBalanceResponse {
+    // Return user's balance if user presented, otherwise will be error response
     public int balance { get; set; }
+    // Return merkle proof nodes if user presented, which are a list of hashes from
+    // sibling nodes when goes up from the matched leaf node to root
+    // root is not included
     public MerkleTree.MerkleProofNode[] proofs { get; set; }
 }
